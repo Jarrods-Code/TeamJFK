f7ef270e7a7fc33de57c0a789913c90ee2677188
KPRepoHub <panxkevin@gmail.com>
2022-02-01 22:07:51 +1100

    use localstorage to persist

--- js/index.js
+++ js/index.js
@@ -32,127 +32,16 @@ function displayTodayDate() {
   dateElement.innerHTML = today.toLocaleDateString("en-AU", dateFormat);
 }
 
-// class Task {
 
-//   constructor(
-//     taskId,
-//     taskName,
-//     taskDescription,
-//     taskStatus,
-//     taskOwner,
-//     taskDueDate
-//   ) {
-//     this.Id = taskId;
-//     this.taskName = taskName;
-//     this.taskDescription = taskDescription;
-//     this.taskStatus = taskStatus;
-//     this.taskOwner = taskOwner;
-
-//     const dateFormat = { year: "numeric", month: "long", day: "numeric" };
-//     const dueDate = new Date(taskDueDate);
-//     const formattedDate = dueDate.toLocaleDateString("en-AU", dateFormat);
-//     this.taskDueDate = formattedDate;
-//   }
-
-//   // setTaskID(id) {
-//   //   this.Id = id;
-//   // }
-
-//   // function addTask
-// }
-
-// class taskManager {
-//   //   static currentID = 0;
-
-//   constructor(currentId = 0) {
-//     this.currentID = currentId;
-
-//     //initialize array to save new tasks
-//     this.taskList = [];
-
-//     console.log(
-//       "value of task array when task Manager is initialized: ",
-//       this.taskList
-//     );
-
-//     console.log("initialize task manager, tasks init state: ", this.taskList);
-//   }
-
-//   validFormFieldInput(
-//     taskName,
-//     taskDescription,
-//     taskOwner,
-//     dueDate,
-//     taskStatus
-//   ) {}
-
-//   //add new task
-//   addTask(taskName, taskDescription, taskStatus, taskOwner, taskDueDate) {
-//     this.currentID += 1;
-
-//     //create task object using task class
-//     const task = new Task(
-//       this.currentID,
-//       taskName,
-//       taskDescription,
-//       taskStatus,
-//       taskOwner,
-//       taskDueDate
-//     );
-
-//     this.taskList.push(task);
-
-//     console.log("new task added to taskList array: ", task);
-//   }
-
-//   createTaskHtml(task) {
-//     let taskStr = `<ul class="list-group" id="tasksList">
-
-//                         <li class="list-group-item">
-//                             <div class="d-flex w-100 mt-2 justify-content-between align-items-center">
-//                                 <h5>${task.taskName}</h5>
-//                                 <span class="badge badge-progress">${task.taskStatus}</span>
-//                             </div>
-//                             <div class="d-flex w-100 mb-3 justify-content-between">
-//                                 <small>Assigned To: ${task.taskOwner}</small>
-//                                 <small>Due: ${task.taskDueDate}</small>
-//                             </div>
-//                             <p>${task.taskDescription}</p>
-//                             <p><button class="edit btn btn-info btn-sm" name="edit"><a
-//                                         style="text-decoration: none; color:white" href="EditTask.html">EDIT</a></button>
-//                                 <button class="delete btn btn-info btn-sm" name="delete">DELETE</button>
-//                             </p>
-//                         </li>
-//                     </ul>`;
-
-//     return taskStr;
-//   }
-
-//   render() {
-//     let tasksHtmlList = [];
-//     let newTasks = document.getElementById("taskManager");
-//     let taskHTML = "";
-
-//     for (let i = 0; i < this.taskList.length; i++) {
-//       let currentTask = this.taskList[i];
-
-//       taskHTML = this.createTaskHtml(currentTask);
-//       console.log(taskHTML);
-
-//       tasksHtmlList.push(taskHTML);
-//     }
-
-//     taskHTML = tasksHtmlList.join("\n");
-
-//     newTasks.innerHTML = taskHTML;
-//   }
-// }
 
 // display today date
 displayTodayDate();
 
 //create task manager
 let taskMgr = new taskManager();
+
+taskMgr.load();
+taskMgr.render();
 
 const form = document.querySelector("#new-task-form");
 
@@ -253,6 +142,7 @@ form.addEventListener("submit", (event) => {
       validateDueDate.value
     );
 
+    // taskMgr.save();
     taskMgr.render();
 
     //clear form ready for next new task
@@ -278,4 +168,33 @@ form.addEventListener("submit", (event) => {
   }
 });
 
-// alert("js ending");
+const taskList = document.querySelector("#task-list");
+
+console.log("task-list start");
+
+  taskList.addEventListener("click", (event) => {
+    console.log("taskList done button event target object", event.target);
+    console.log("done-button ", event.target.classList.contains("done-button"));
+
+     if  (event.target.classList.contains("done-button")) {
+       console.log("done button clicked ");
+       const parentElement = event.target.parentElement.parentElement;
+
+       console.log("parent element ", parentElement);
+
+       //get the task id from the parentElement element
+       const taskId = Number(parentElement.dataset.taskId);
+       console.log("select task id ", taskId);
+
+       //find the task in the task array
+       const returnedTask = taskMgr.getTaskById(taskId);
+
+       console.log("returned task ", returnedTask);
+     }
+  }
+);
+
+console.log("task-list end");
+
+
+
